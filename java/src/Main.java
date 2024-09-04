19a9079df644ab7497686d57d2d99427b801499c
Ubuntu <ubuntu@ip-172-31-11-90.ap-southeast-2.compute.internal>
2024-09-04 14:11:01 +0000

    Initial commit

---
+++ java/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+	public static void main(String[]args){
+		Date currentDate=new Date();
+		System.out.println("Hello,  Bhaiyo aur Behno! Current Date: " +currentDate );
+		}
+	}
+
